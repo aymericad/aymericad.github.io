b06cd0f39708b2bf88977c25b5db43230834486c
Vincent LAURENT <vince.laurent@gmail.com>
2015-07-20 02:23:56 +0200

    Navigation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 $(window).load(function() {
-  $(".circle").each(function(i, that) {
-      drawCircle($(that));
-  });
+    $(".circle").each(function(i, that) {
+        drawCircle($(that));
+    });
+
+    $("#nav_menu_open").click(function() {
+        $('#menu').show();
+        $('html').addClass('no-scroll');
+
+        return false;
+    });
+
+    $("#nav_menu_close").click(function() {
+        $('#menu').hide();
+        $('html').removeClass('no-scroll');
+
+        return false;
+    });
 });
 
 function drawCircle(circle) {
   var size = 100 - circle.parent().attr("data-size"); //Récupération de la valeur size
   var coor = [[], []];
   size = (size / 100) * 180; //Calcul de l'angle en degrès
-  console.log(size);
   coor[0] = polarToCartesian(90, 90, 84, 270 - size); //Coordonnées de l'extremité gauche
   coor[1] = polarToCartesian(90, 90, 84, 270); //Coordonnées de l'extremité droite
   circle.attr("d", "M " + coor[0][0] + "," + coor[0][1] + " A 84 84 0 " + (( 100 - circle.parent().attr("data-size") > 50) ? 0 : 1) +" 0 " + coor[1][0] + "," + coor[1][1]); //Affectation de la valeur au path SVG
